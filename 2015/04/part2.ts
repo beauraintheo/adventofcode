3c4f5c55fcfaec575df933482a6bba983707a0bd
Théo Beaurain <theobeaurain@mbp-de-theo.home>
2024-01-24 20:28:33 +0100

    complete part 1 & 2 day 4 - 2015

---
+++ 2015/04/part2.ts
@@ -0,0 +1,10 @@
+import { hashedWord } from "./utils";
+
+/**
+ * Find the lowest positive number that produces a hash with 6 leading zeroes
+ * @param file Array of strings containing the directions
+ * @returns The lowest positive number that produces a hash with 6 leading zeroes
+ */
+const part2 = (file: string[]): number => hashedWord(file[0], "000000", 0);
+
+export default part2;
